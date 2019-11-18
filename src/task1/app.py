bafb138cdfa5d38262ec8702521f0d8e5c594b3f
Madiy <dmovsky.max20@yandex.by>
2019-11-18 17:51:13 +0300

    Scrolling and moving update
    Added initial normal size of window
    Added scrolling and moving by mouse

--- src/task1/app.py
+++ src/task1/app.py
@@ -1,10 +1,9 @@
 import sys
 import os
 from PyQt5.QtWidgets import (QWidget, QApplication, QLabel, QScrollArea)
-from PyQt5.QtGui import QPainter, QBrush, QPen, QFont
-from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPainter, QBrush, QPen, QFont, QTransform
+from PyQt5.QtCore import Qt,QPoint
 from helper import graph_from_json
-
 
 # TODO, not save shortest edge.
 class Application(QWidget):
@@ -14,12 +13,18 @@ class Application(QWidget):
         self.padding = 50
         self.points_labels = {}
         self.max_node_radius = 50
+        self.zoom=1
+        self.delta=QPoint()
+        self.delta.setX=0
+        self.delta.setY=0
+        self.start =self.delta
+        self.mydelta=self.delta
         self.initLabels()
         self.initUI()
-
+        self.pressing = False
 
     def initUI(self):
-        self.showMaximized()
+        #self.showMaximized()
         self.setGeometry(500, 500, self.width() // 2, self.height() // 2)
         self.setWindowTitle('Graph visualization')
         self.show()
@@ -31,12 +36,15 @@ class Application(QWidget):
 
     def paintEvent(self, e):
         painter = QPainter()
-        painter.begin(self)
-
+        painter.begin(self)     
         x_coef = self.width() - 2 * self.padding
         y_coef = self.height() - 2 * self.padding
+        pos = self.graph.pos        
+        painter.scale(self.zoom,self.zoom)
+        painter.translate(self.delta)
+        self.delta.setX=self.start.x()
+        self.delta.setY=self.start.y()   
 
-        pos = self.graph.pos
 
         # maybe do property ?
         for edge in self.graph.edges.values():
@@ -65,9 +73,31 @@ class Application(QWidget):
             font.setPointSize(font_size)
             lbl.setFont(font)
             lbl.adjustSize()
-            self.points_labels[idx].move(x - lbl.width() // 2, y - lbl.height() // 2)
-
+            self.points_labels[idx].move(self.zoom*(x - lbl.width() // 2 + self.delta.x()), self.zoom*(y - lbl.height() // 2+self.delta.y()))
+                  
         painter.end()
+
+
+    def wheelEvent(self, event):
+        self.zoom += event.angleDelta().y()/2880
+        self.update()
+        
+    def mousePressEvent(self, QMouseEvent):
+        #the pos of the widget when you first pressed it.
+        self.start = QMouseEvent.pos()-self.delta
+        self.beg_pos=[self.start.x(),self.start.y()]
+        #to make sure you are holding mouse button down
+        self.pressing = True
+
+    def mouseMoveEvent(self, QMouseEvent):
+        if self.pressing:
+            self.end = QMouseEvent.pos()
+            self.delta = (self.end-self.start)
+            self.end = self.start
+            self.update()
+
+    def mouseReleaseEvent(self, QMouseEvent):
+        self.pressing = False    
 
 
 if __name__ == '__main__':
@@ -80,4 +110,5 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     graph = graph_from_json(filename)
     win = Application(graph)
+    win.resize(500,500)
     sys.exit(app.exec_())
