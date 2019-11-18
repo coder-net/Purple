fb835649577bb221197a937c820082ffb4110b88
coder-net <for.different.question@gmail.com>
2019-11-19 00:06:21 +0300

    code refactor

--- src/task1/app.py
+++ src/task1/app.py
@@ -1,88 +1,93 @@
 import sys
 import os
-from PyQt5.QtWidgets import (QWidget, QApplication, QLabel, QScrollArea,QInputDialog,QFileDialog, QPushButton,QLineEdit,QGridLayout,QVBoxLayout )
-    #QMainWindow, QDesktopWidget,   QAction
-from PyQt5.QtGui import QPainter, QBrush, QPen, QFont
-from PyQt5.QtCore import Qt,QPoint
+from PyQt5.QtWidgets import (QWidget, QApplication, QLabel, QFileDialog, QPushButton, QLineEdit, QGridLayout,
+                             QVBoxLayout)
+from PyQt5.QtGui import QPainter, QBrush, QPen
+from PyQt5.QtCore import Qt, QPoint
 from helper import graph_from_json
+
 
 class Application(QWidget):
     def __init__(self):
         super().__init__()
-        self.initUI()        
+        self.initUI()
 
     def initUI(self):
-        # Selecter input file
+        # TODO: main window to center
+        self.setGeometry(500, 500, 1000, 800)
         file_label = QLabel("file", self)
         file_label.resize(file_label.sizeHint())
-        file_label.move(20,5)
+        # why
+        file_label.move(20, 5)
         file_label.setToolTip('end file ')
         self.filePath = QLineEdit("Select file", self)
         self.filePath.setObjectName("file")
         self.filePath.resize(self.filePath.sizeHint())
         self.filePath.move(120, 5)
         self.filePath.setToolTip('Select file')
-        file_select_bytton = QPushButton('Select file', self)
-        file_select_bytton.resize(file_select_bytton.sizeHint())
-        file_select_bytton.move(300, 5)
-        file_select_bytton.clicked.connect(lambda: self.selectFile()) 
-        
+        # const size bad
+        file_select_button = QPushButton('Select file', self)
+        file_select_button.resize(file_select_button.sizeHint())
+        file_select_button.move(300, 5)
+        file_select_button.clicked.connect(self.selectFile)
+
         home_button = QPushButton('Home', self)
         home_button.resize(home_button.sizeHint())
-        home_button.clicked.connect(lambda: self.cameraToHome())         
-        
+        home_button.clicked.connect(self.cameraToHome)
+
         grid = QGridLayout()
-        grid.addWidget(file_label,1,0)
-        grid.addWidget(self.filePath,1,1)
-        grid.addWidget(file_select_bytton,1,2)
-        grid.addWidget(home_button,2,0)
-        
-        
+        grid.setHorizontalSpacing(10)
+        grid.addWidget(file_label, 1, 1)
+        grid.addWidget(self.filePath, 1, 2)
+        grid.addWidget(file_select_button, 1, 3)
+        grid.addWidget(home_button, 1, 0)
+
         verticalLayout = QVBoxLayout()
         verticalLayout.setSpacing(0)
         graph_drawer = GraphDrawer()
-        graph_drawer.resize(500,500)        
-        self.graphWidget=graph_drawer  
+        graph_drawer.resize(500, 500)
+        self.graphWidget = graph_drawer
         verticalLayout.addLayout(grid)
         verticalLayout.addWidget(self.graphWidget)
         self.setLayout(verticalLayout)
         self.show()
 
-
+    # TODO, error handing, if file not chosen
     def selectFile(self):
         self.filePath.setText(QFileDialog.getOpenFileName()[0])
-        self.graphWidget.graph = graph_from_json(self.filePath.text())   
-        if len(self.graphWidget.points_labels)>0:
-            for idx in self.graphWidget.points_labels:    
-                self.graphWidget.points_labels[idx].hide();
-            self.graphWidget.points_labels.clear();
-        self.graphWidget.initLabels()   
-    
+        self.graphWidget.graph = graph_from_json(self.filePath.text())
+        if len(self.graphWidget.points_labels) > 0:
+            for idx in self.graphWidget.points_labels:
+                self.graphWidget.points_labels[idx].hide()
+            self.graphWidget.points_labels.clear()
+        self.graphWidget.initLabels()
+
+    # TODO: incapsulate
     def cameraToHome(self):
         if self.graphWidget.graph:
-            self.graphWidget.zoom=1
-            self.graphWidget.delta=QPoint(0,0)
-            self.graphWidget.start=self.graphWidget.delta
+            self.graphWidget.zoom = 1
+            self.graphWidget.delta = QPoint(0, 0)
+            self.graphWidget.start = self.graphWidget.delta
             self.graphWidget.update()
+
 
 class GraphDrawer(QWidget):
     def __init__(self):
         super().__init__()
         self.graph = None
         self.padding = 100
+        # TODO: rename points_to_labels
         self.points_labels = {}
-        self.max_node_radius = 50
-        self.zoom=1
-        self.delta=QPoint(0, 0)
-        self.start =self.delta
-        self.initUI()
+        self.zoom = 1
+        self.delta = QPoint(0, 0)
+        self.start = self.delta
         self.pressing = False
+        self.initUI()
 
     def initUI(self):
         self.setGeometry(500, 500, self.width() // 2, self.height() // 2)
         self.setWindowTitle('Graph visualization')
         self.show()
-
 
     def initLabels(self):
         for idx in self.graph.points:
@@ -92,17 +97,17 @@ class GraphDrawer(QWidget):
     def paintEvent(self, e):
         if self.graph:
             painter = QPainter()
-            painter.begin(self)     
+            painter.begin(self)
+            painter.scale(self.zoom, self.zoom)
+            painter.translate(self.delta)
+
+            self.delta.setX = self.start.x()
+            self.delta.setY = self.start.y()
+
             x_coef = self.width() - 2 * self.padding
             y_coef = self.height() - 2 * self.padding
-            pos = self.graph.pos        
-            painter.scale(self.zoom,self.zoom)
-            painter.translate(self.delta)
-            self.delta.setX=self.start.x()
-            self.delta.setY=self.start.y()   
+            pos = self.graph.pos
 
-
-            # maybe do property ?
             for edge in self.graph.edges.values():
                 p1, p2 = edge.points
                 painter.drawLine(
@@ -126,35 +131,33 @@ class GraphDrawer(QWidget):
                 painter.drawEllipse(x - r // 2, y - r // 2, r, r)
                 lbl = self.points_labels[idx]
                 font = lbl.font()
-                font.setPointSize(font_size*self.zoom)
+                font.setPointSize(font_size * self.zoom)
                 lbl.setFont(font)
                 lbl.adjustSize()
-                self.points_labels[idx].move(self.zoom*(x -r/5 + self.delta.x()), self.zoom*(y-r/5+self.delta.y()))
+                lbl.move(self.zoom * (x + self.delta.x()) - lbl.width() // 2,
+                         self.zoom * (y + self.delta.y()) - lbl.height() // 2)
 
-            painter.end()        
-
+            painter.end()
 
     def wheelEvent(self, event):
-        self.zoom += event.angleDelta().y()/2880
+        self.zoom += event.angleDelta().y() / 2880
         self.update()
 
     def mousePressEvent(self, QMouseEvent):
-        self.start = QMouseEvent.pos()-self.delta
-        self.beg_pos=[self.start.x(),self.start.y()]
+        self.start = QMouseEvent.pos() - self.delta
         self.pressing = True
 
     def mouseMoveEvent(self, QMouseEvent):
         if self.pressing:
             end = QMouseEvent.pos()
-            self.delta = (end-self.start)
+            self.delta = (end - self.start)
             self.update()
 
     def mouseReleaseEvent(self, QMouseEvent):
-        self.pressing = False    
+        self.pressing = False
 
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    win=Application()
-    win.resize(600,800)
+    win = Application()
     sys.exit(app.exec_())
