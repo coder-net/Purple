889c1f0f46c7b5b4428a7fb893f8ea43282d1174
Maksim Abramovich <z.silence@yandex.ru>
2019-11-25 04:54:15 +0300

    RequestHandler in implementation

--- src/task2/connection.py
+++ src/task2/connection.py
@@ -24,11 +24,14 @@ class Result(enum.Enum):
     TIMEOUT = 5
     INTERNAL_SERVER_ERROR = 500
 
-class ResponseError(Exception):
-    def __init__(self, result, error):
-        self.result = result
+class GameConnectionError(Exception):
+    def __init__(self, code, error):
+        self.code = code
         self.error = error
-        super().__init__(": ".join((result.name, error)))
+        super().__init__(": ".join((code.name, error)))
+
+class ResponseError(GameConnectionError):
+    pass
 
 class Connector:
     int_size = 4
@@ -101,28 +104,49 @@ class Connector:
         return data
 
     def send(self, action, data=""):
-        assert isinstance(action, Action)
+        assert isinstance(action, Action), "action is not valid."
         action_code = __class__.from_int(action.value)
         data_bytes = data.encode(self.encoding)
         length_code = __class__.from_int(len(data_bytes))
         msg = b"".join((action_code, length_code, data_bytes))
         self.socket.sendall(msg)
 
+class RequestError(GameConnectionError):
+    pass
+
 class RequestHandler:
     def __init__(self, connector=Connector()):
-        self.connector = connector
+        self.cn = connector
+        self.map_idx = 0
 
-    def login(self, name, **options):
-        pass
+    def login(self, obj):
+        if self.cn: raise RequestError(Action.LOGIN, "Already login.")
+        self.cn.connect()
+        player = self.cn.request(Action.LOGIN, obj)
+        return player
 
     def player(self):
-        pass
+        if not self.cn: raise RequestError(Action.PLAYER, "Please Login first.")
+        player = self.cn.request(Action.PLAYER)
+        return player
 
     def logout(self):
-        pass
+        if not self.cn: raise RequestError(Action.LOGOUT, "Not login.")
+        self.cn.request(Action.LOGOUT)
+        self.cn.close()
 
-    def map(self, layer):
-        pass
+    def map(self, smap):
+        if not self.cn: raise RequestError(Action.PLAYER, "Please Login first.")
+        if not self.map_idx:
+            smap = self.cn.request(Action.MAP, dict(layer=0))
+            xy = self.cn.request(Action.MAP, dict(layer=10))
+            # TODO, update static map by coordinates
+            # assert smap["idx"] == xy["idx"]
+            # smap["size"] = xy["size"]
+            # for idx, x, y in (point.values() for point in xy["coordinates"]):
+            #     pass # way to indexing in list by points idx
+        dmap = self.cn.request(Action.MAP, dict(layer=1))
+        # update smap by dmap: points[idx].post_idx <- posts[post_idx], where idx equal
 
 
 def connector_demonstration():
@@ -185,5 +209,5 @@ def send_some_requests():
 
 
 if __name__ == "__main__":
-    # connector_demonstration()
+    connector_demonstration()
     send_some_requests()
